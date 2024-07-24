0a80a028913a62b1b464d2ba3c9d07986b14cb12
HuyPham1a <144685387+HuyPham1a@users.noreply.github.com>
2024-07-24 19:39:28 +0700

    exam

---
+++ Exam/SearchWord.cs
@@ -0,0 +1,28 @@
+﻿namespace SearchWord
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a String: ");
+            string str = Console.ReadLine();
+            Console.Write("Enter a word to search: ");
+            string word = Console.ReadLine();
+            int count = 0;
+            int index = str.IndexOf(word);
+            while (index != -1)
+            {
+                count++;
+                index = str.IndexOf(word, index + word.Length);
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("Word not found.");
+            }
+            else
+            {
+                Console.WriteLine("Word found {0} times in the string.", count);
+            }
+        }
+    }
+}
