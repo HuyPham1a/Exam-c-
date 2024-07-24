73682385435cef81c0b1b147efb2b30c9309391b
HuyPham1a <144685387+HuyPham1a@users.noreply.github.com>
2024-07-24 19:39:28 +0700

    exam

---
+++ Exam/People.cs
@@ -0,0 +1,39 @@
+﻿namespace Exam;
+
+class People
+{
+    private string name;
+    private bool gender;
+    private int age;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public bool Gender
+    {
+        get { return gender; }
+        set { gender = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public People(string name, bool gender, int age)
+    {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {name}, Gender: Female, Age: {age}";
+    }
+}
+
---
+++ Exam/Program.cs
@@ -0,0 +1,13 @@
+﻿// namespace Exam;
+//
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         
+//         People person = new People("Marry", true, 25);
+//         Console.WriteLine("Person details: " + person);
+//         person.Age++;
+//         Console.WriteLine("Person details (apter incrementing age): " + person);
+//     }
+// }
